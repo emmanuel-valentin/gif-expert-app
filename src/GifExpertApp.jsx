42abb12282b000703b359acc9d6fce32f443ecb2
Emmanuel Guadalupe <ema_110@outlook.es>
2023-02-20 22:32:42 -0600

    Gif Expert App terminada

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+import trimInputValue from './helpers/trimInputValue';
+
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(trimInputValue(newCategory))) return;
+    setCategories([
+      trimInputValue(newCategory),
+      ...categories
+    ]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory onNewCategory={onAddCategory} />
+      {categories.map(category => <GifGrid key={category} category={category} />)}
     </>
   );
 };

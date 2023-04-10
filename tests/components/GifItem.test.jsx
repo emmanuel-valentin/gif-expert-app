e80292d154dab355f28d21dc28ac1b00c882dbf4
Emmanuel Valentin <ema_110@outlook.es>
2023-04-09 18:59:38 -0600

    Add GifItem tests

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,12 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import GifItem from '../../src/components/GifItem';
 
 describe('Pruebas en el componente GifItem', () => {
+  const title = "Hola mundo";
+  const image = { url: 'https://www.holamundo.com/' };
+
   test('Debe hacer match con el snapshot', () => {
-    const title = "Hola mundo";
-    const image = { url: 'https://www.holamundo.com' };
     const { container } = render(<GifItem title={title} image={image} />);
-    
     expect(container).toMatchSnapshot();
+  });
+
+  test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GifItem title={title} image={image} />);
+    // screen.debug();
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(image.url);
+    expect(alt).toBe(title);
+  });
+
+  test('Debe de mostrar el título en el componente', () => {
+    render(<GifItem title={title} image={image} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
